8c3006bc20d9e4b2b53d319d0b2581d8589c1c4a
Maksim Bartoshyk <maksim_bartoshyk@epam.com>
2025-12-15 17:43:56 +0300

    feat (mca): vibe-code basic PWA

---
+++ mca/service-worker.js
@@ -0,0 +1,48 @@
+// MCA PWA Service Worker
+// Version format: YYYYMMDD.n (increment n for same-day releases)
+const CACHE_VERSION = '20251215.2';
+const CACHE_NAME = `mca-cache-${CACHE_VERSION}`;
+
+const ASSETS_TO_CACHE = [
+  './mca.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install: pre-cache all assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting()) // Activate immediately
+  );
+});
+
+// Activate: delete old caches and take control
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((name) => name.startsWith('mca-cache-') && name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        );
+      })
+      .then(() => self.clients.claim()) // Take control of all clients
+  );
+});
+
+// Fetch: cache-first strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
