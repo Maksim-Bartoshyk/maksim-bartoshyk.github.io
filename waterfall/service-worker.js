42334102636450f5ee6689d9bed4af846a2db1fd
Maksim Bartoshyk <maksim_bartoshyk@epam.com>
2026-02-25 20:06:17 +0300

    feat (waterfall): add initial PWA for waterfall renderer

---
+++ waterfall/service-worker.js
@@ -0,0 +1,51 @@
+// PWA Service Worker
+// Version format: vYYYYMMDD.n (increment n for same-day releases)
+// WARN: duplicates version in html title, need to keep in sync manually
+const CACHE_VERSION = 'v20260225.1';
+const CACHE_NAME = `wf-cache-${CACHE_VERSION}`;
+
+const ASSETS_TO_CACHE = [
+  './waterfall.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// install: pre-cache all assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting()) // activate immediately
+  );
+});
+
+// activate: delete old caches and take control
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((name) => name.startsWith('wf-cache-') && name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        );
+      })
+      .then(() => self.clients.claim()) // take control of all clients
+  );
+});
+
+// fetch: cache-first strategy
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
